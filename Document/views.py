0e699e67aff9ead6b2d446d936a8c171f96d6be5
honk0128 <honk0128@gmail.com>
2023-05-26 23:23:52 +0900

    ver1.6

--- Document/views.py
+++ Document/views.py
@@ -1,3 +1,6 @@
+
+
+
 import re
 from django.shortcuts import render, redirect
 from .models import Document, File
@@ -9,23 +12,24 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def document_upload(request):
+    
+    document_types = [1, 2, 3]    
     user = request.user
     try:
         employee = Employee.objects.get(Emp_User=user)
     except Employee.DoesNotExist:
         return redirect('login')
 
-    document_types = [1, 2, 3]  # you may need to replace this with the actual document types
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             form.instance.Doc_Dept = employee.Emp_Dept
-            form.instance.Doc_Type = request.POST.get('Doc_Type')
+            # form.instance.Doc_Type = request.POST.get('Doc_Type')
             form.save()
             return redirect('/document_upload?success_page=true')
         else:
             print(form.errors)
     else:
-        form = DocumentForm(initial={'Doc_Dept': employee.Emp_Dept})
+        form = DocumentForm()
 
     return render(request, 'fileupload.html', {'form': form, 'employee': employee, 'document_types': document_types})
