aab4c3321ecf4ec0565a1e459ad4879eaada7b1e
honk0128 <honk0128@gmail.com>
2023-05-26 17:12:40 +0900

    ver1.2

--- Document/views.py
+++ Document/views.py
@@ -4,7 +4,27 @@ from .models import Document, File
 from .forms import DocumentForm
 from Emp.models import Employee, Department
 
-
+# def document_upload(request):
+#     if request.method == 'POST':
+#         form = DocumentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             document = Document()
+#             user = Employee.objects.get(Emp_User = request.user)
+#             document.Doc_ID = 1
+#             document.Doc_Dept = user.Emp_Dept
+#             document.Doc_Sender = request.POST.get('Doc_Sender')
+#             document.Doc_Receiver = request.POST.get('Doc_Receiver')
+#             document.Doc_Title = request.POST.get('Doc_Title')
+#             document.Doc_Type = request.POST.get('Doc_Type')
+#             document.Doc_State = 2
+#             document.save()
+#             return redirect('/document_upload?success_page=true')
+#         else:
+#             print(form.errors) 
+#     else:
+#         form = DocumentForm()
+#     return render(request, 'fileupload.html', {'form': form})
 
 
 def document_upload(request):
